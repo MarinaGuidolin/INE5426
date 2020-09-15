5b08f203812f9dbf5692a065ed793a95aecd5772
Marina Guidolin <marinaguidolin@hotmail.com>
2020-09-15 19:21:19 -0300

    Antlr MyLexer on main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        try {
+            System.out.println("input");
+            ANTLRInputStream input = new ANTLRInputStream(System.in);
+            MyLexer lexer = new MyLexer(input);
+           
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
