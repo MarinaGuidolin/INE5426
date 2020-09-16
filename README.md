48b4aa8e73b648290d592840876be42fda3e4fb6
Luiz Motta <luizjoaomotta@gmail.com>
2020-09-16 01:28:28 -0300

    Use args to make the lexycal analysis

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # INE5426
 
 ## Executando o projeto:
-#### `mvn clean install`
-#### `mvn compile`
-#### `mvn exec:java`
+#### `mvn clean`
+#### `mvn package`
+#### `mvn exec:java -Dexec.args="<filepath>"`
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,18 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {  
         try {
+            if (args.length > 1) {
+                throw new Error("Should have only one argument");
+            }
+
+            String filePath = args[0];
+            // extract file extension and check if it's .ccc, and throw an error
+
             System.out.println("Starting Lexical Analysis");
 
-            CharStream charStream = CharStreams.fromFileName("./examples/example1.ccc");
+            CharStream charStream = CharStreams.fromFileName(filePath);
             CC2020Lexer lexer = new CC2020Lexer(charStream);
             CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
             CC2020Parser parser = new CC2020Parser(commonTokenStream);
@@ -17,6 +24,7 @@ public class Main {
            
             System.out.println("Done");
         } catch (IOException e) {
+            // handle the CharStream.fromFileName()
             e.printStackTrace();
         }
     }
