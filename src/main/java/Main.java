dd9656bb3ae802a89b35e5651bd231297c05cf77
Luiz Motta <luizjoaomotta@gmail.com>
2020-09-16 02:11:45 -0300

    Add error handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,23 @@
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {  
+        final String VALID_FILE_EXTENSION = "ccc";
+
         try {
             if (args.length > 1) {
                 throw new Error("Should have only one argument");
             }
 
             String filePath = args[0];
-            // extract file extension and check if it's .ccc, and throw an error
+            if (!Utils.extractFileExtension(filePath).equals(VALID_FILE_EXTENSION)) {
+                throw new Error("Invalid file extension. It should be a .ccc file!");
+            }
 
             System.out.println("Starting Lexical Analysis");
 
@@ -20,11 +25,22 @@ public class Main {
             CC2020Lexer lexer = new CC2020Lexer(charStream);
             CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
             CC2020Parser parser = new CC2020Parser(commonTokenStream);
+            
             parser.program();
-           
-            System.out.println("Done");
+            int errorTotal = parser.getNumberOfSyntaxErrors();
+            System.out.println("Lexical analysis finished with: " + errorTotal + (errorTotal == 1 ? " error." : " errors"));
+
+            if (errorTotal == 0) {
+                System.out.println("Well done!");
+            }
+            
         } catch (IOException e) {
-            // handle the CharStream.fromFileName()
+            System.out.println("Invalid file");
+            e.printStackTrace();
+        } catch (RecognitionException e) {
+            System.out.println("Error while parsing the file");
+            e.printStackTrace();
+        } catch (Error e) {
             e.printStackTrace();
         }
     }
---
+++ src/main/java/Utils.java
@@ -0,0 +1,10 @@
+public class Utils {
+    static public String extractFileExtension(String filePath) {
+        String extension = "";
+        int i = filePath.lastIndexOf('.');
+        if (i > 0) {
+            extension = filePath.substring(i + 1);
+        }
+        return extension;
+    }
+}
